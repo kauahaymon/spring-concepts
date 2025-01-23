8344a3d3154677afccaab11577109fdbd5600ed6
Kauã Haymon <kauahaymon@hotmail.com>
2025-01-22 22:45:25 -0400

    feat: put, delete product

--- src/main/java/haymon/world/spring_concepts/controller/ProductController.java
+++ src/main/java/haymon/world/spring_concepts/controller/ProductController.java
@@ -1,9 +1,14 @@
 package haymon.world.spring_concepts.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,10 +23,29 @@ public class ProductController {
 	private ProductRepository repository;
 
 	@PostMapping
-	public @ResponseBody Product newProduct(@RequestParam 
-			String name, double price, double discount) {
-		Product product = new Product(name, price, discount);
+//	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
+	public @ResponseBody Product newProduct(Product product) {
 		repository.save(product);
 		return product;
+	}
+	
+	@GetMapping
+	public Iterable<Product> getAllProducts() {
+		return repository.findAll();
+	}
+	
+	@GetMapping(path = "/{id}")
+	public Optional<Product> getProductById(@PathVariable int id) {
+		return repository.findById(id);
+	}
+	
+	@PutMapping
+	public Product updateProduct(Product product) {
+		return repository.save(product);
+	}
+	
+	@DeleteMapping(path = "/{id}")
+	public void deleteProductById(@PathVariable int id) {
+		repository.deleteById(id);
 	}
 }
